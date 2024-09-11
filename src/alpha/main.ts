7c307bfb0b7e034bdc4290492635b5c6ed8089f9
TrungThanh12081997 <quantrungthanh73@gmail.com>
2024-09-11 14:34:01 +0700

    fix

--- src/alpha/main.ts
+++ src/alpha/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from '@src/alpha/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+
   await app.listen(8888);
+
 }
 bootstrap();
