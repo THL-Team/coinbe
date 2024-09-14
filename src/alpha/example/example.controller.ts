861cba67acedd50088fc7f4a43b34b4c66c2c9f1
TrungThanh12081997 <quantrungthanh73@gmail.com>
2024-09-14 17:07:01 +0700

    17:06 test build

--- src/alpha/example/example.controller.ts
+++ src/alpha/example/example.controller.ts
@@ -15,7 +15,7 @@ import puppeteer from 'puppeteer';
 
 @Controller('example')
 export class ExampleController {
-  constructor(private readonly exampleService: ExampleService) {}
+  constructor(private readonly exampleServicessss: ExampleService) {}
 
   @Post()
   create(@Body() createExampleDto: CreateExampleDto) {
